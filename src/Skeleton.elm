20301ceaa4ec28f6428d37274cf7b40ff44adaa4
Yoshitaka Totsuka <y047aka@gmail.com>
2022-08-28 01:09:51 +0900

    refactor: Remove UI.Container and UI.Segment

--- src/Skeleton.elm
+++ src/Skeleton.elm
@@ -1,17 +1,17 @@
 module Skeleton exposing (skeleton)
 
-import Css exposing (backgroundColor, borderBottom3, displayFlex, hex, int, justifyContent, padding, position, px, solid, spaceBetween, sticky, top, zIndex, zero)
+import Css exposing (..)
 import Css.FontAwesome exposing (fontAwesome)
 import Css.Global exposing (global)
+import Css.Media as Media exposing (only, screen, withMedia)
+import Css.Palette as Palette exposing (darkPalette, palette, setBackground, setColor)
 import Css.Reset exposing (normalize)
 import Css.ResetAndCustomize exposing (additionalReset, globalCustomize)
 import Data.Theme as Theme exposing (Theme(..))
-import Html.Styled as Html exposing (Html, div, header, main_, option, select, text)
+import Html.Styled as Html exposing (Attribute, Html, div, header, option, select, text)
 import Html.Styled.Attributes exposing (css, selected, value)
 import Html.Styled.Events exposing (onInput)
 import UI.Breadcrumb exposing (BreadcrumbItem, Divider(..), breadcrumbWithProps)
-import UI.Container exposing (container)
-import UI.Segment exposing (basicSegment)
 
 
 skeleton : { model | theme : Theme } -> { changeThemeMsg : Theme -> msg } -> List (Html msg) -> Html msg
@@ -19,7 +19,7 @@ skeleton model { changeThemeMsg } body =
     div []
         [ global (normalize ++ additionalReset ++ globalCustomize ++ fontAwesome)
         , siteHeader model { changeThemeMsg = changeThemeMsg } { title = "title" }
-        , main_ [] [ basicSegment { theme = Light } [] [ container [] body ] ]
+        , main_ { theme = Light } [] body
         ]
 
 
@@ -57,3 +57,47 @@ breadcrumbItems { title } =
             [ { label = "Top", url = "/" }
             , { label = title, url = "" }
             ]
+
+
+main_ : { theme : Theme } -> List (Attribute msg) -> List (Html msg) -> Html msg
+main_ { theme } attributes children =
+    Html.styled Html.main_
+        [ margin zero
+        , padding zero
+        , palette Palette.init
+        , darkPalette theme
+            (Palette.init
+                |> setBackground (hex "#1B1C1D")
+                |> setColor (rgba 255 255 255 0.9)
+            )
+        ]
+        attributes
+        [ container [] children ]
+
+
+container : List (Attribute msg) -> List (Html msg) -> Html msg
+container =
+    Html.styled Html.div
+        [ maxWidth (pct 100)
+
+        -- Mobile
+        , withMedia [ only screen [ Media.maxWidth (px 559.98) ] ]
+            [ width auto
+            , marginLeft (em 1)
+            , marginRight (em 1)
+            ]
+
+        -- Tablet
+        , withMedia [ only screen [ Media.minWidth (px 560), Media.maxWidth (px 1199.98) ] ]
+            [ width auto
+            , marginLeft (em 3)
+            , marginRight (em 3)
+            ]
+
+        -- Large Monitor
+        , withMedia [ only screen [ Media.minWidth (px 1200) ] ]
+            [ width (px 1120)
+            , marginLeft auto
+            , marginRight auto
+            ]
+        ]
