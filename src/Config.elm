c232dc4f6381c860eeba71fb974f04548749e9ba
Yoshitaka Totsuka <y047aka@gmail.com>
2022-08-29 22:04:34 +0900

    Add `Radio`

--- src/Config.elm
+++ src/Config.elm
@@ -1,7 +1,7 @@
 module Config exposing (Config(..), configToField)
 
-import Html.Styled as Html exposing (Html, div, input, select, text)
-import Html.Styled.Attributes exposing (selected, type_, value)
+import Html.Styled as Html exposing (Html, div, input, label, select, text)
+import Html.Styled.Attributes exposing (checked, for, id, name, selected, type_, value)
 import Html.Styled.Events exposing (onClick, onInput)
 import UI.Button exposing (button, labeledButton)
 import UI.Checkbox as Checkbox
@@ -12,6 +12,14 @@ import UI.Label exposing (basicLabel)
 type Config option msg
     = String { label : String, value : String, onInput : String -> msg }
     | Bool { id : String, label : String, bool : Bool, onClick : msg }
+    | Radio
+        { name : String
+        , value : option
+        , options : List option
+        , fromString : String -> Maybe option
+        , toString : option -> String
+        , onChange : option -> msg
+        }
     | Select
         { value : option
         , options : List option
@@ -42,6 +50,29 @@ configToField config =
                 , checked = c.bool
                 , onClick = c.onClick
                 }
+
+        Radio c ->
+            div [] <|
+                List.map
+                    (\option ->
+                        let
+                            prefixedId =
+                                c.name ++ "_" ++ c.toString option
+                        in
+                        div []
+                            [ input
+                                [ id prefixedId
+                                , type_ "radio"
+                                , name c.name
+                                , value (c.toString option)
+                                , checked (c.value == option)
+                                , onInput (c.fromString >> Maybe.withDefault c.value >> c.onChange)
+                                ]
+                                []
+                            , label [ for prefixedId ] [ text (c.toString option) ]
+                            ]
+                    )
+                    c.options
 
         Select c ->
             select [ onInput (c.fromString >> Maybe.withDefault c.value >> c.onChange) ] <|
